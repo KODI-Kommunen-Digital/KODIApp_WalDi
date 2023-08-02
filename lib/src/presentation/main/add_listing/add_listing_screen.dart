2156d909d9e4788831fb34fc728e1f12a961dbc4
Deniosj <denioshaji28@gmail.com>
2023-08-02 08:37:38 +0200

    Remove duplicate address field in add listing

--- lib/src/presentation/main/add_listing/add_listing_screen.dart
+++ lib/src/presentation/main/add_listing/add_listing_screen.dart
@@ -44,7 +44,6 @@ class _AddListingScreenState extends State<AddListingScreen> {
   final _textPlaceController = TextEditingController();
 
   final _focusTitle = FocusNode();
-  final _focusPlace = FocusNode();
   final _focusContent = FocusNode();
   final _focusAddress = FocusNode();
   final _focusZipCode = FocusNode();
@@ -615,20 +614,6 @@ class _AddListingScreenState extends State<AddListingScreen> {
             //   ],
             // ),
             // const SizedBox(height: 16),
-            AppTextInput(
-              hintText: Translate.of(context).translate('input_place'),
-              controller: _textPlaceController,
-              focusNode: _focusPlace,
-              textInputAction: TextInputAction.next,
-              onSubmitted: (text) {
-                Utils.fieldFocusChange(
-                  context,
-                  _focusPlace,
-                  _focusAddress,
-                );
-              },
-            ),
-            const SizedBox(height: 8),
             AppTextInput(
               hintText: Translate.of(context).translate('input_address'),
               // errorText: _errorAddress,
