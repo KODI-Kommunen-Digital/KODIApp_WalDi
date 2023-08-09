335c1ddd5884b869ac73a739749f5c419bf705a4
DanielFratczak <daniel@heidi-app.de>
2023-08-09 17:02:03 +0200

    Updating citizen services based on chosen city (+MultiDex enabled)

--- lib/src/presentation/main/discovery/discovery_screen.dart
+++ lib/src/presentation/main/discovery/discovery_screen.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:heidi/src/data/model/model_citizen_service.dart';
 import 'package:heidi/src/presentation/cubit/app_bloc.dart';
+import 'package:heidi/src/presentation/main/home/home_screen/cubit/home_cubit.dart';
+import 'package:heidi/src/presentation/main/home/home_screen/cubit/home_state.dart';
 import 'package:heidi/src/utils/configs/image.dart';
 import 'package:heidi/src/utils/configs/preferences.dart';
 import 'package:heidi/src/utils/configs/routes.dart';
@@ -25,35 +28,49 @@ class _DiscoveryScreenState extends State<DiscoveryScreen> {
 
   final List<CitizenServiceModel> hiddenServices = [];
 
-  final List<CitizenServiceModel> services = [
-    CitizenServiceModel(imageUrl: Images.service1, imageLink: "1"),
-    CitizenServiceModel(imageUrl: Images.service2, imageLink: "2"),
-    CitizenServiceModel(
-        imageUrl: Images.service3,
-        imageLink: "3",
-        type: "subCategoryService",
-        arguments: 4),
-    CitizenServiceModel(
-        imageUrl: Images.service4, imageLink: "4", arguments: 4, categoryId: 1),
-    CitizenServiceModel(
-        imageUrl: Images.service5, imageLink: "5", arguments: 5, categoryId: 3),
-    CitizenServiceModel(
-        imageUrl: Images.service6, imageLink: "6", arguments: 6, categoryId: 4),
-    CitizenServiceModel(
-        imageUrl: Images.service7,
-        imageLink: "7",
-        arguments: 7,
-        categoryId: 10),
-    CitizenServiceModel(
-        imageUrl: Images.service8,
-        imageLink: "8",
-        arguments: 8,
-        categoryId: 13),
-    CitizenServiceModel(
-        imageUrl: Images.service9, imageLink: "9", arguments: 9, categoryId: 6),
-  ];
+  late List<CitizenServiceModel> services;
 
   Future<void> hideEmptyService() async {
+    services = [
+      CitizenServiceModel(imageUrl: Images.service1, imageLink: "1"),
+      CitizenServiceModel(imageUrl: Images.service2, imageLink: "2"),
+      CitizenServiceModel(
+          imageUrl: Images.service3,
+          imageLink: "3",
+          type: "subCategoryService",
+          arguments: 4),
+      CitizenServiceModel(
+          imageUrl: Images.service4,
+          imageLink: "4",
+          arguments: 4,
+          categoryId: 1),
+      CitizenServiceModel(
+          imageUrl: Images.service5,
+          imageLink: "5",
+          arguments: 5,
+          categoryId: 3),
+      CitizenServiceModel(
+          imageUrl: Images.service6,
+          imageLink: "6",
+          arguments: 6,
+          categoryId: 4),
+      CitizenServiceModel(
+          imageUrl: Images.service7,
+          imageLink: "7",
+          arguments: 7,
+          categoryId: 10),
+      CitizenServiceModel(
+          imageUrl: Images.service8,
+          imageLink: "8",
+          arguments: 8,
+          categoryId: 13),
+      CitizenServiceModel(
+          imageUrl: Images.service9,
+          imageLink: "9",
+          arguments: 9,
+          categoryId: 6),
+    ];
+
     for (var element in services) {
       if (element.categoryId != null || element.type == "subCategoryService") {
         bool hasContent = await element.hasContent();
@@ -75,27 +92,32 @@ class _DiscoveryScreenState extends State<DiscoveryScreen> {
         centerTitle: true,
         title: Text(Translate.of(context).translate('cust_services')),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // Adjust the number of columns as desired
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-            mainAxisExtent: 300.0),
-        itemCount: services.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {
-              navigateToLink(services[index]);
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(15.0),
-              child: Image.asset(
-                services[index].imageUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-          );
+      body: BlocListener<HomeCubit, HomeState>(
+        listener: (context, state) {
+          hideEmptyService();
         },
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Adjust the number of columns as desired
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              mainAxisExtent: 300.0),
+          itemCount: services.length,
+          itemBuilder: (BuildContext context, int index) {
+            return InkWell(
+              onTap: () {
+                navigateToLink(services[index]);
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.asset(
+                  services[index].imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
