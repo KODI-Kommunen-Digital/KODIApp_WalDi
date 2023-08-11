2ad21302ed7732d6cf9920c7544ea5c6685ed652
Deniosj <denioshaji28@gmail.com>
2023-08-11 08:08:24 +0200

    Base Url Change

--- lib/src/data/remote/api/http_manager.dart
+++ lib/src/data/remote/api/http_manager.dart
@@ -16,7 +16,6 @@ class HTTPManager {
     _dio = Dio(
       BaseOptions(
         baseUrl: 'https://app.smartregion-auf.de/api',
-        // baseUrl: 'http://localhost:8002',
         connectTimeout: 30000,
         receiveTimeout: 30000,
         contentType: Headers.formUrlEncodedContentType,
