38fd8df3d498045d03a82b72c757d5d04d12f013
DanielFratczak <daniel@heidi-app.de>
2023-08-16 16:30:35 +0200

    BaseURL change

--- lib/src/data/remote/api/http_manager.dart
+++ lib/src/data/remote/api/http_manager.dart
@@ -15,7 +15,8 @@ class HTTPManager {
   HTTPManager() {
     _dio = Dio(
       BaseOptions(
-        baseUrl: 'https://test.smartregion-auf.de/api',
+        baseUrl: 'https://waldi.app/api',
+        //baseUrl: 'https://test.smartregion-auf.de/api',
         connectTimeout: 30000,
         receiveTimeout: 30000,
         contentType: Headers.formUrlEncodedContentType,
