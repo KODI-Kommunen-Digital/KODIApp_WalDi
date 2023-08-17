2acc27b3ad5f9e4afd1adf89b7f01272eddb5d6e
SaAF-HEIDI <saud@heidi-app.de>
2023-08-17 10:09:50 +0200

    update base Urls

--- lib/src/utils/configs/application.dart
+++ lib/src/utils/configs/application.dart
@@ -8,8 +8,8 @@ class Application {
   static SettingModel setting = SettingModel.fromDefault();
 
   static const String picturesURL =
-  'https://test1heidi.obs.eu-de.otc.t-systems.com/';
-      // 'https://smrauf1heidi.obs.eu-de.otc.t-systems.com/';
+  // 'https://test1heidi.obs.eu-de.otc.t-systems.com/';
+      'https://smrauf1heidi.obs.eu-de.otc.t-systems.com/';
 
   static final Application _instance = Application._internal();
 
