1c459cd4b2d47b7234eabac0a7d6d537efd9985d
SaAF-HEIDI <saud@heidi-app.de>
2023-08-21 12:57:23 +0200

    Add Change password screen validation

--- lib/src/presentation/main/account/change_password/change_password_screen.dart
+++ lib/src/presentation/main/account/change_password/change_password_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:heidi/src/presentation/cubit/app_bloc.dart';
 import 'package:heidi/src/presentation/widget/app_button.dart';
@@ -19,6 +20,9 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   final _focusPass = FocusNode();
   final _focusNewPass = FocusNode();
 
+  bool _showPassword = false;
+  bool _showCPassword = false;
+  bool _isPasswordFocused = false;
   String? _errorPass;
   String? _errorNewPass;
 
@@ -39,13 +43,13 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   void _changePassword() async {
     Utils.hiddenKeyboard(context);
     setState(() {
-      _errorPass = UtilValidator.validate(
-        _textPassController.text,
-      );
-      _errorNewPass = UtilValidator.validate(
-        _textNewPassController.text,
-      );
+      _errorPass =  AppBloc.signupCubit.validatePassword(_textPassController.text);
+      _errorNewPass = UtilValidator.validate(_textNewPassController.text,
+          password: _textPassController.text, type: ValidateType.cpassword);
     });
+    if (_errorPass != null) {
+      _errorPass = Translate.of(context).translate(_errorPass);
+    }
     if (_errorPass == null && _errorNewPass == null) {
       final result = await AppBloc.changePasswordCubit.onChangePassword(
         _textPassController.text,
@@ -60,6 +64,9 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
+    setPasswordListener();
+    String passwordHint = Translate.of(context).translate('Password hint');
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -88,16 +95,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                     'input_your_password',
                   ),
                   errorText: _errorPass,
-                  focusNode: _focusPass,
-                  textInputAction: TextInputAction.next,
-                  obscureText: true,
-                  onSubmitted: (text) {
-                    Utils.fieldFocusChange(
-                      context,
-                      _focusPass,
-                      _focusNewPass,
-                    );
-                  },
                   onChanged: (text) {
                     setState(() {
                       _errorPass = UtilValidator.validate(
@@ -105,7 +102,35 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                       );
                     });
                   },
+                  onSubmitted: (text) {
+                    Utils.fieldFocusChange(
+                      context,
+                      _focusPass,
+                      _focusNewPass,
+                    );
+                  },
+                  trailing: GestureDetector(
+                    dragStartBehavior: DragStartBehavior.down,
+                    onTap: () {
+                      setState(() {
+                        _showPassword = !_showPassword;
+                      });
+                    },
+                    child: Icon(_showPassword
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                  ),
+                  obscureText: !_showPassword,
                   controller: _textPassController,
+                  focusNode: _focusPass,
+                ),
+                const SizedBox(height: 16),
+                Visibility(
+                  visible: _isPasswordFocused,
+                  child: Text(
+                    passwordHint,
+                    style: const TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
                 ),
                 const SizedBox(height: 16),
                 Text(
@@ -123,11 +148,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                   ),
                   errorText: _errorNewPass,
                   focusNode: _focusNewPass,
-                  textInputAction: TextInputAction.done,
-                  obscureText: true,
-                  onSubmitted: (text) {
-                    _changePassword();
-                  },
                   onChanged: (text) {
                     setState(() {
                       _errorNewPass = UtilValidator.validate(
@@ -135,6 +155,21 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                       );
                     });
                   },
+                  onSubmitted: (text) {
+                    _changePassword();
+                  },
+                  trailing: GestureDetector(
+                    dragStartBehavior: DragStartBehavior.down,
+                    onTap: () {
+                      setState(() {
+                        _showCPassword = !_showCPassword;
+                      });
+                    },
+                    child: Icon(_showCPassword
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                  ),
+                  obscureText: !_showCPassword,
                   controller: _textNewPassController,
                 ),
                 const SizedBox(height: 16),
@@ -149,5 +184,13 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
         ),
       ),
     );
+  }
+
+  void setPasswordListener() {
+    _focusPass.addListener(() {
+      setState(() {
+        _isPasswordFocused = _focusPass.hasFocus;
+      });
+    });
   }
 }
