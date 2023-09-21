23d650e9c1f6bab4b634cc6a8ac865e09a440810
SaAF-HEIDI <saud@heidi-app.de>
2023-09-21 16:34:47 +0200

    ask for photos permission in IOS

--- lib/src/presentation/widget/app_upload_image.dart
+++ lib/src/presentation/widget/app_upload_image.dart
@@ -10,6 +10,7 @@ import 'package:heidi/src/data/repository/list_repository.dart';
 import 'package:heidi/src/utils/configs/application.dart';
 import 'package:heidi/src/utils/multiple_gesture_detector.dart';
 import 'package:heidi/src/utils/translate.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 enum UploadImageType { circle, square }
@@ -35,6 +36,7 @@ class AppUploadImage extends StatefulWidget {
 }
 
 class _AppUploadImageState extends State<AppUploadImage> {
+  final _picker = ImagePicker();
   File? _file;
   double? _percent;
   bool isImageUploaded = false;
@@ -54,6 +56,7 @@ class _AppUploadImageState extends State<AppUploadImage> {
 
   Future<void> showChooseFileTypeDialog() async {
     PermissionStatus status;
+
     if (await Permission.storage.isGranted) {
       status = PermissionStatus.granted;
     } else {
@@ -103,31 +106,55 @@ class _AppUploadImageState extends State<AppUploadImage> {
               SimpleDialogOption(
                 onPressed: () async {
                   Navigator.pop(context);
-                  FilePickerResult? result =
-                      await FilePicker.platform.pickFiles(
-                    type: FileType.image,
+                  // FilePickerResult? result =
+                  //     await FilePicker.platform.pickFiles(
+                  //   type: FileType.image,
+                  // );
+                  // if (result != null) {
+                  //   _file = File('');
+                  //   setState(() {
+                  //     _file = File(result.files.single.path!);
+                  //     isImageUploaded = false;
+                  //   });
+                  //   final profile = widget.profile;
+                  //   if (!profile) {
+                  //     await ListRepository.uploadImage(_file!, profile);
+                  //     widget.onChange('image');
+                  //   } else {
+                  //     final response =
+                  //         await ListRepository.uploadImage(_file!, profile);
+                  //     if (response!.data['status'] == 'success') {
+                  //       setState(() {
+                  //         isImageUploaded = true;
+                  //       });
+                  //       final item = response.data['data']?['image'];
+                  //       widget.onChange(item);
+                  //     }
+                  //   }
+                  // }
+
+                  final pickedFile = await _picker.pickImage(
+                    source: ImageSource.gallery,
                   );
-                  if (result != null) {
-                    _file = File('');
-                    setState(() {
-                      _file = File(result.files.single.path!);
-                      isImageUploaded = false;
-                    });
-                    final profile = widget.profile;
-                    if (!profile) {
-                      await ListRepository.uploadImage(_file!, profile);
-                      widget.onChange('image');
-                    } else {
-                      final response =
-                          await ListRepository.uploadImage(_file!, profile);
-                      if (response!.data['status'] == 'success') {
-                        setState(() {
-                          isImageUploaded = true;
-                        });
-                        final item = response.data['data']?['image'];
-                        widget.onChange(item);
-                      }
-                    }
+                  if (pickedFile == null) return;
+                  if (!mounted) return;
+                  setState(() {
+                    isImageUploaded = false;
+                    _file = File(pickedFile.path);
+                  });
+                  final profile = widget.profile;
+                  if (!profile) {
+                    await ListRepository.uploadImage(_file!, profile);
+                  } else {
+                    final response =
+                        await ListRepository.uploadImage(_file!, profile);
+                    if (response!.data['status'] == 'success') {
+                      setState(() {
+                        isImageUploaded = true;
+                      });
+                      final item = response.data['data']?['image'];
+                      widget.onChange(item);
+                    } else {}
                   }
                 },
                 child: ListTile(
