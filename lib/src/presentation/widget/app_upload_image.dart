c61a8619e52722f6d236b5c7b90f7fe459a4c18f
SaAF-HEIDI <saud@heidi-app.de>
2023-09-22 10:57:07 +0200

    update IOS photos permissions

--- lib/src/presentation/widget/app_upload_image.dart
+++ lib/src/presentation/widget/app_upload_image.dart
@@ -133,28 +133,64 @@ class _AppUploadImageState extends State<AppUploadImage> {
                   //   }
                   // }
 
-                  final pickedFile = await _picker.pickImage(
-                    source: ImageSource.gallery,
-                  );
-                  if (pickedFile == null) return;
-                  if (!mounted) return;
-                  setState(() {
-                    isImageUploaded = false;
-                    _file = File(pickedFile.path);
-                  });
-                  final profile = widget.profile;
-                  if (!profile) {
-                    await ListRepository.uploadImage(_file!, profile);
-                  } else {
-                    final response =
-                        await ListRepository.uploadImage(_file!, profile);
-                    if (response!.data['status'] == 'success') {
+                  if (Platform.isIOS) {
+                    if (await Permission.photos.isGranted ||
+                        await Permission.photos.isLimited) {
+                      status = PermissionStatus.granted;
+                      final pickedFile = await _picker.pickImage(
+                        source: ImageSource.gallery,
+                      );
+                      if (pickedFile == null) return;
+                      if (!mounted) return;
                       setState(() {
-                        isImageUploaded = true;
+                        isImageUploaded = false;
+                        _file = File(pickedFile.path);
                       });
-                      final item = response.data['data']?['image'];
-                      widget.onChange(item);
-                    } else {}
+                      final profile = widget.profile;
+                      if (!profile) {
+                        await ListRepository.uploadImage(_file!, profile);
+                        widget.onChange('image');
+                      } else {
+                        final response =
+                            await ListRepository.uploadImage(_file!, profile);
+                        if (response!.data['status'] == 'success') {
+                          setState(() {
+                            isImageUploaded = true;
+                          });
+                          final item = response.data['data']?['image'];
+                          widget.onChange(item);
+                        }
+                      }
+                    } else if (await Permission.photos.isDenied) {
+                      await Permission.photos.request();
+                    } else if (await Permission.photos.isPermanentlyDenied) {
+                      status = PermissionStatus.permanentlyDenied;
+                      await openAppSettings();
+                    }
+                  } else {
+                    final pickedFile = await _picker.pickImage(
+                      source: ImageSource.gallery,
+                    );
+                    if (pickedFile == null) return;
+                    if (!mounted) return;
+                    setState(() {
+                      isImageUploaded = false;
+                      _file = File(pickedFile.path);
+                    });
+                    final profile = widget.profile;
+                    if (!profile) {
+                      await ListRepository.uploadImage(_file!, profile);
+                    } else {
+                      final response =
+                          await ListRepository.uploadImage(_file!, profile);
+                      if (response!.data['status'] == 'success') {
+                        setState(() {
+                          isImageUploaded = true;
+                        });
+                        final item = response.data['data']?['image'];
+                        widget.onChange(item);
+                      } else {}
+                    }
                   }
                 },
                 child: ListTile(
