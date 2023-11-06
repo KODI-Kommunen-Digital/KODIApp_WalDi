a40d2ead7859ef9dc21fba037c8d59c8f6482ac1
SaAF-HEIDI <saud@heidi-app.de>
2023-11-06 10:17:02 +0100

    solve image permission bug

--- lib/src/presentation/widget/app_upload_image.dart
+++ lib/src/presentation/widget/app_upload_image.dart
@@ -147,14 +147,28 @@ class _AppUploadImageState extends State<AppUploadImage> {
     if (await Permission.storage.isGranted) {
       status = PermissionStatus.granted;
     } else {
-      String androidVersion = await _getAndroidVersion();
-      if (int.parse(androidVersion) < 11) {
-        status = await Permission.storage.status;
-      } else {
-        status = await Permission.photos.status;
+      if (Platform.isAndroid) {
+        final androidInfo = await DeviceInfoPlugin().androidInfo;
+        if (androidInfo.version.sdkInt <= 32) {
+          status = await Permission.storage.status;
+          status = await Permission.storage.request();
+        }  else {
+          status = await Permission.photos.status;
+          status = await Permission.photos.request();
+        }
       }
-      status = await Permission.storage.request();
-      status = await Permission.photos.request();
+      else{
+        status = await Permission.photos.status;
+        status = await Permission.photos.request();
+      }
+      // String androidVersion = await _getAndroidVersion();
+      // if (int.parse(androidVersion) < 11) {
+      //   status = await Permission.storage.status;
+      //   status = await Permission.storage.request();
+      // } else {
+      //   status = await Permission.photos.status;
+      //   status = await Permission.photos.request();
+      // }
     }
 
     if (!mounted) return;
@@ -222,7 +236,7 @@ class _AppUploadImageState extends State<AppUploadImage> {
                           widget.onChange(item);
                         }
                       }
-                    } else if (await Permission.photos.isDenied) {
+                    } else if (await Permission.photos.isDenied || await Permission.storage.isDenied) {
                       await Permission.photos.request();
                     } else if (await Permission.photos.isPermanentlyDenied) {
                       status = PermissionStatus.permanentlyDenied;
