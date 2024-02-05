8095e8201605ebaa920c4efbfb6e6fad3e49eeac
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-02-05 11:02:48 +0100

    Add Terms of Use

--- lib/src/presentation/main/account/legal/legal.dart
+++ lib/src/presentation/main/account/legal/legal.dart
@@ -76,6 +76,23 @@ class _LegalScreenState extends State<LegalScreen> {
                 ],
               ),
             ),
+            AppListTitle(
+              title: Translate.of(context).translate('terms_of_use'),
+              onPressed: () {
+                _makeAction('https://app.smartregion-auf.de/TermsOfUse');
+              },
+              trailing: Row(
+                children: <Widget>[
+                  RotatedBox(
+                    quarterTurns: AppLanguage.isRTL() ? 2 : 0,
+                    child: const Icon(
+                      Icons.keyboard_arrow_right,
+                      textDirection: TextDirection.ltr,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
