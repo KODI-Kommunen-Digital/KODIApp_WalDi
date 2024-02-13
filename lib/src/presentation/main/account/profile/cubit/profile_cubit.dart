aff9177a713d62c2151da0e3b5ffb7f9ed9a98fb
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-02-13 11:03:35 +0100

    Subategory bug fix, Remove unused images

--- lib/src/presentation/main/account/profile/cubit/profile_cubit.dart
+++ lib/src/presentation/main/account/profile/cubit/profile_cubit.dart
@@ -66,6 +66,7 @@ class ProfileCubit extends Cubit<ProfileState> {
             pdf: product.pdf,
             category: product.category,
             categoryId: product.categoryId,
+            subcategoryId: product.subcategoryId,
             startDate: product.startDate,
             endDate: product.endDate,
             createDate: product.createDate,
--- lib/src/utils/configs/image.dart
+++ lib/src/utils/configs/image.dart
@@ -1,21 +1,6 @@
 class Images {
-  static const String intro1 = "assets/images/intro_1.png";
-  static const String intro2 = "assets/images/intro_2.png";
-  static const String intro3 = "assets/images/intro_3.png";
   static const String logo = "assets/images/logo.png";
   static const String slider = "assets/images/slider.jpeg";
-  static const String whatsapp = "assets/images/whatsapp.png";
-  static const String telegram = "assets/images/telegram.png";
-  static const String viber = "assets/images/viber.png";
-  static const String facebook = "assets/images/facebook.png";
-  static const String flickr = "assets/images/flickr.png";
-  static const String google = "assets/images/google.png";
-  static const String linkedin = "assets/images/linkedin.png";
-  static const String pinterest = "assets/images/pinterest.png";
-  static const String youtube = "assets/images/youtube.png";
-  static const String twitter = "assets/images/twitter.png";
-  static const String tumblr = "assets/images/tumblr.png";
-  static const String instagram = "assets/images/instagram.png";
   static const String service1 = "assets/images/services-1.png";
   static const String service2 = "assets/images/services-2.png";
   static const String service3 = "assets/images/services-3.png";
