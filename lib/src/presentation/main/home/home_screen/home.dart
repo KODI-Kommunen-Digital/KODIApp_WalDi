e12ebe60d8f4763e94b2c898ebf5cabcc03ae612
DS-John-HEIDI <130041934+DS-John-HEIDI@users.noreply.github.com>
2024-03-22 10:39:20 +0100

    Release changes

--- lib/src/presentation/main/home/home_screen/home.dart
+++ lib/src/presentation/main/home/home_screen/home.dart
@@ -225,7 +225,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         String? minAppVersion}) {
                       if (display != null) {
                         setState(() {
-                          latestAppStoreVersion = appStoreVersion ?? '1.10';
+                          latestAppStoreVersion = appStoreVersion ?? '1.11';
                         });
                       }
                     },
